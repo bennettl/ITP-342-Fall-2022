fcc5b46c652e93b97093549bbd43fa0012feb582
Bennett Lee <bennettlee@Bennett-Lee-QGV9HFMHKL.local>
2022-08-28 22:24:34 -0700

    week 1

---
+++ Week1/MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Bennett Lee on 8/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
