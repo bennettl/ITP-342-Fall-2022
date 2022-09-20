a18c5f290fc9e83629a91c1441b15652212b4e83
Bennett Lee <bennettlee@Bennett-Lee-QGV9HFMHKL.local>
2022-09-20 14:49:22 -0700

    week updates

--- Week4/AnotherRandomQuoteGenerator/AnotherRandomQuoteGenerator/Controller/ViewController.swift
+++ Week4/AnotherRandomQuoteGenerator/AnotherRandomQuoteGenerator/Controller/ViewController.swift
@@ -8,13 +8,45 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+// Step 1: Conform to the protocol of the delegate
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var mainTextField: UITextField!
+    @IBOutlet weak var firstNumberTextField: UITextField!
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    
+    @IBOutlet weak var sumLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        mainTextField.delegate = self
+       
+        
+//        UIApplication.shared.
+//        FileManager.default
+        
     }
 
-
+    // Step 2: Implement the methods we want to subscribe to
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("\(#function)")
+        return true
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        print("\(textField.text?.uppercased())")
+    }
+    
+    @IBAction func addDidTapped(_ sender: UIButton) {
+        let firstNumber = Int(firstNumberTextField.text!)!
+        let secondNumber = Int(secondNumberTextField.text!)!
+        let result = add(firstNumber: firstNumber, with: secondNumber)
+        sumLabel.text = "\(result)"
+    }
+    
+    func add(firstNumber: Int, with secondNumber: Int) -> Int{
+        let result = firstNumber - secondNumber
+        return result
+    }
 }
 
--- Week4/Protocols.playground/Contents.swift
+++ Week4/Protocols.playground/Contents.swift
@@ -20,6 +20,11 @@ protocol Drivable {
 }
 
 class Car : Drivable {
+  
+    func honk() {
+        
+    }
+    
     func turnLeft() {
         print("turning left as a car")
     }
@@ -27,7 +32,7 @@ class Car : Drivable {
     func turnRight() {
         
     }
-    
+   
     func accelerate(speed: Int) {
         
     }
@@ -69,5 +74,34 @@ class Sedan : Car, ABC {
 
 let mazda : Drivable = Car()
 let blueBike = Bike()
+let benz: Drivable = Sedan()
 
+
+
+
+
+// Singleton: Have an object alive / in memory throughout the lifetime of the app
+// Reason: This is an object that we are 1) Using very frequently and 2) Using across multiple screens or 3) Sometimes it makes sense to only have one instance of the job
+// Example: Current User, UIApplication
+
+struct User {
+    let firstName: String
+    
+    let lastName: String
+}
+
+class UserManager {
+    
+    // Current user needs to be a singleton
+    static var current: User? = User(firstName: "Bennett", lastName: "Lee")
+    
+
+}
+
+
+UserManager.current // singleton
+UserManager.current = User(firstName: "Sami", lastName: "G")
+UserManager.current
+UserManager.current
+UserManager.current
 
